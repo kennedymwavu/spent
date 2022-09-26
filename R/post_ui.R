7ee95e04e5ac2a7ec42307599b0a6c1867d63859
kennedymwavu <mwavukennedy@gmail.com>
2022-09-26 22:46:59 +0300

    style div details

--- R/post_ui.R
+++ R/post_ui.R
@@ -8,38 +8,50 @@ post_ui <- function(id) {
   ns <- NS(namespace = id)
   
   shiny::tagList(
-    tags$h3('Please fill in the details below:'), 
-    textInput(
-      inputId = ns('fullnames'), 
-      label = 'Full Names',
-      # value = 'John Wick'
-      placeholder = 'eg. John Wick'
-    ), 
-    
-    textInput(
-      inputId = ns('email'), 
-      label = 'Email:', 
-      # value = 'myemail@email.com'
-      placeholder = 'eg. johnwick@gmail.com'
-    ), 
-    
-    textInput(
-      inputId = ns('phone'), 
-      label = 'Phone Number:', 
-      # value = '+254712345672'
-      placeholder = 'eg. +254712345672'
-    ), 
-    
-    textInput(
-      inputId = ns('address'), 
-      label = 'Address', 
-      # value = 'po box 256'
-    ), 
-    
-    shinyFeedback::loadingButton(
-      inputId = ns('submit'), 
-      label = 'Submit', 
-      loadingLabel = 'Submitting...'
+    fluidRow(
+      align = 'center', 
+      class = 'mycontainer', 
+      
+      tags$h3('Please fill in the details below:'), 
+      
+      tags$div(
+        class = 'details', 
+        
+        textInput(
+          inputId = ns('fullnames'), 
+          label = 'Full Names',
+          placeholder = 'John Wick', 
+          width = '500px'
+        ), 
+        
+        textInput(
+          inputId = ns('email'), 
+          label = 'Email:', 
+          placeholder = 'johnwick@gmail.com', 
+          width = '500px'
+        ), 
+        
+        textInput(
+          inputId = ns('phone'), 
+          label = 'Phone Number:', 
+          placeholder = '+254712345672', 
+          width = '500px'
+        ), 
+        
+        textInput(
+          inputId = ns('address'), 
+          label = 'Address', 
+          placeholder = 'Afya Center, Nairobi | 337407', 
+          width = '500px'
+        )
+      ), 
+      
+      shinyFeedback::loadingButton(
+        inputId = ns('submit'), 
+        label = 'Submit', 
+        loadingLabel = 'Submitting...', 
+        class = 'btn btn-info'
+      )
     )
   )
 }
