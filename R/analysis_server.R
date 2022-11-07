e52573326ab8d50e59956382a8232deb36553580
kennedymwavu <mwavukennedy@gmail.com>
2022-11-07 16:12:25 +0300

    render plots directly without using *Output fns, more efficient; value boxes for monthly stats

--- R/analysis_server.R
+++ R/analysis_server.R
@@ -9,21 +9,7 @@ analysis_server <- function(id, post_url) {
   moduleServer(
     id = id, 
     module = function(input, output, session) {
-      output$plt_amt_per_month <- echarts4r::renderEcharts4r({
-        plt_amt_per_month
-      })
       
-      output$plt_top_n_items <- echarts4r::renderEcharts4r({
-        plt_top_n_items
-      })
-      
-      output$plt_store_freq <- echarts4r::renderEcharts4r({
-        plt_store_freq
-      })
-      
-      output$plt_hr_freq <- echarts4r::renderEcharts4r({
-        plt_hr_freq
-      })
     }
   )
 }
--- R/analysis_ui.R
+++ R/analysis_ui.R
@@ -10,19 +10,47 @@ analysis_ui <- function(id) {
   shiny::tagList(
     fluidRow(
       align = 'center', 
-      class = 'mycontainer', 
+      class = 'mycontainer',
       
-      tags$div(
-        class = 'plots', 
-        
-        echarts4r::echarts4rOutput(outputId = ns(id = 'plt_amt_per_month')), 
-        
-        echarts4r::echarts4rOutput(outputId = ns(id = 'plt_top_n_items')), 
-        
-        echarts4r::echarts4rOutput(outputId = ns(id = 'plt_store_freq')), 
-        
-        echarts4r::echarts4rOutput(outputId = ns(id = 'plt_hr_freq'))
-      )
+      bslib::value_box(
+        title = 'Average Per Month',
+        value = format_currency(avg_per_month),
+        showcase = icon(
+          name = 'hand-holding-dollar', 
+          class = 'fa-icon fa-hand-holding-dollar'
+        ),
+        showcase_layout = bslib::showcase_left_center(), 
+        full_screen = FALSE, 
+        theme_color = 'primary'
+      ), 
+      
+      bslib::value_box(
+        title = 'Least Expenditure Month',
+        value = least_spending_month[, format_currency(c(amount))],
+        showcase = icon(name = 'down-long', class = 'down-solid fa-up-long'), 
+        showcase_layout = bslib::showcase_left_center(), 
+        tags$p(least_spending_month[, c(month)]), 
+        full_screen = FALSE, 
+        theme_color = 'primary'
+      ), 
+      
+      bslib::value_box(
+        title = 'Highest Expenditure Month',
+        value = highest_spending_month[, format_currency(c(amount))],
+        showcase = icon(name = 'up-long', class = 'fa-solid fa-up-long'), 
+        showcase_layout = bslib::showcase_left_center(), 
+        tags$p(highest_spending_month[, c(month)]), 
+        full_screen = FALSE, 
+        theme_color = 'primary'
+      ), 
+      
+      plt_amt_per_month, 
+      
+      plt_top_n_items, 
+      
+      plt_store_freq, 
+      
+      plt_hr_freq
     )
   )
 }
