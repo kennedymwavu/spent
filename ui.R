b484135a8a1fa10ae5e9efe0a8cfc3dd9abb7f18
kennedymwavu <mwavukennedy@gmail.com>
2022-11-08 23:05:12 +0300

    switch to bootswatch 'flatly'

--- ui.R
+++ ui.R
@@ -1,5 +1,5 @@
 ui <- bslib::page(
-  theme = bslib::bs_theme(version = 5, bootswatch = 'zephyr'),
+  theme = bslib::bs_theme(version = 5, bootswatch = 'flatly'),
   title = 'Spent', 
   
   shinyjs::useShinyjs(), 
