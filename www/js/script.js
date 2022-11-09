d727d0018a9b402d0a146a70d67ddf2f3af8ce42
kennedymwavu <mwavukennedy@gmail.com>
2022-11-09 07:34:03 +0300

    faders js

--- www/js/script.js
+++ www/js/script.js
@@ -1,3 +1,28 @@
 function get_id(clicked_id) {
   Shiny.setInputValue("records-current_id", clicked_id, {priority: "event"});
 }
+
+shinyjs.init = function() {
+  const faders = document.querySelectorAll('.fade-in');
+
+  const appearOptions = {
+    threshold: 0, 
+    rootMargin: '0px 0px -200px 0px'
+  };
+
+  // observer:
+  const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add('appear');
+        appearOnScroll.unobserve(entry.target);
+      }
+  })
+  }, appearOptions);
+
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  })
+}
